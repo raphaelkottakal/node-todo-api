ae7b5d8ce54aabdcf180b32870d1b4ba6e8b604c
Raphael Kottakal <raphael@webmanifestation.com>
2018-10-15 17:49:26 +0530

    add todos delete route

--- server/server.js
+++ server/server.js
@@ -48,6 +48,21 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findByIdAndDelete(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.send({ todo });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+})
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
