bc93df796bf5141d5a2c81c5e57fb3f90e155e88
Raphael Kottakal <raphael@webmanifestation.com>
2018-10-15 18:03:37 +0530

    add delete /todos route tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,4 +98,40 @@ describe('GET /todos/:id', () => {
       .expect(404)
       .end(done);
   });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    const hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toBeFalsy();
+          done();
+        }).catch(err => done(err));
+      })
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    const id = new ObjectID();
+    request(app)
+      .delete(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete(`/todos/1234`)
+      .expect(404)
+      .end(done);
+  });
 })
