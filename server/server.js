4ce1bc0486714be33696aa0a6cb06b2b52362e1f
Raphael Kottakal <raphael@webmanifestation.com>
2018-10-15 18:49:00 +0530

    add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -61,6 +62,30 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((err) => {
     res.status(400).send();
   });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, { new: true }).then((todo) => {
+    if (!todo) {
+      res.status(404).send()
+    }
+    res.send(todo);
+  }).catch(err => {
+    res.status(400).send();
+  })
 })
 
 app.listen(port, () => {
